d4f7b629c377e497699e91772914b21dcb68be09
Christian Meredith <ceigey@users.noreply.github.com>
2019-09-29 13:56:44 +0930

    Setting up clang-format and vscode

--- 01-window.cpp
+++ 01-window.cpp
@@ -9,58 +9,56 @@ using namespace std;
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[]) {
-	// Window to render to
-	SDL_Window *window = NULL;
-	// Surface contained by the window
-	SDL_Surface *screenSurface = NULL;
-	
-	//Initialise
-	if (SDL_Init(SDL_INIT_VIDEO) < 0 ) {
-		// printf("SDL init karked it. SDL_Error: %s\n", SDL_GetError());
-		cout
-		<< "SDL init karked it. SDL_Error: "
-		<< SDL_GetError() << "\n";
-	} else {
-		cout << "• SDL Initialised. \n";
-		window = SDL_CreateWindow(
-			"SDL_Tutorial",
-			SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED,
-			SCREEN_WIDTH,
-			SCREEN_HEIGHT,
-			SDL_WINDOW_SHOWN
-		);
-		if (window == NULL) {
-			cout
-			<< "SDL Window won't work. SDL_Error: "
-			<< SDL_GetError() << "\n";
-		} else {
-			cout << "• SDL Window created. \n";
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect(
-				screenSurface,
-				NULL,
-				SDL_MapRGB(
-					screenSurface->format,
-					0xFF, 0xFF, 0xFF
-				)
-			);
-			SDL_UpdateWindowSurface(window);
-			cout << "  • SDL Window updated. \n";
-			bool quit = false;
-			SDL_Event e;
-			// On Mac you need an event loop
-			// for the window to even show!
-			while (!quit && SDL_PollEvent(&e) != 0) {
-				SDL_Delay(2000);
-				quit = true;
-			}
-		}
-	}
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-	cout << "• SDL Quitting... \n";
-	return 0;
-}
+int main(int argc, char* args[])
+{
+    // Window to render to
+    SDL_Window* window = NULL;
+    // Surface contained by the window
+    SDL_Surface* screenSurface = NULL;
 
+    // Initialise
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        // printf("SDL init karked it. SDL_Error: %s\n", SDL_GetError());
+        cout << "SDL init karked it. SDL_Error: " << SDL_GetError() << "\n";
+    } else {
+        cout << "• SDL Initialised. \n";
+
+        window = SDL_CreateWindow(
+            "SDL_Tutorial",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN);
+
+        if (window == NULL) {
+            cout << "SDL Window won't work. SDL_Error: " << SDL_GetError() << "\n";
+        } else {
+            cout << "• SDL Window created. \n";
+
+            screenSurface = SDL_GetWindowSurface(window);
+
+            SDL_FillRect(
+                screenSurface,
+                NULL,
+                SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+            SDL_UpdateWindowSurface(window);
+
+            cout << "  • SDL Window updated. \n";
+
+            bool quit = false;
+            SDL_Event e;
+            // On Mac you need an event loop
+            // for the window to even show!
+            while (!quit && SDL_PollEvent(&e) != 0) {
+                SDL_Delay(2000);
+                quit = true;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    cout << "• SDL Quitting... \n";
+    return 0;
+}
--- README.md
+++ README.md
@@ -5,4 +5,10 @@ This is NOT related to the Lazyfoo SDL tutorials.
 For the actual tutorials, see:
 [http://lazyfoo.net/tutorials/SDL/index.php](http://lazyfoo.net/tutorials/SDL/index.php)
 
+## Useful links
 
+* [https://code.visualstudio.com/docs/cpp/c-cpp-properties-schema-reference](https://code.visualstudio.com/docs/cpp/c-cpp-properties-schema-reference)
+
+## Useful commands
+
+* `clang-format -style=llvm -dump-config > .clang-format` where `llvm` could also be `google`.
